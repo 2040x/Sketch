4e7f930a9a66655c0f4fb592d8ffea0365c99e84
Ian van Steen <ianvansteen@gmail.com>
2018-11-30 14:42:16 -0800

    Etch-a-Sketch based off of Nick Kessler's

---
+++ Project_Javascript_jQuery.js
@@ -0,0 +1,35 @@
+function createGrid(x) {
+    for (var rows = 0; rows < x; rows++) {
+        for (var columns = 0; columns < x; columns++) {
+            $("#container").append("<div class='grid'></div>");
+        };
+    };
+    $(".grid").width(960/x);
+    $(".grid").height(960/x);
+};
+
+function clearGrid(){
+    $(".grid").remove();
+};  
+
+function refreshGrid(){
+    clearGrid();
+    createGrid(16);
+};
+
+$(document).ready(function() {
+    createGrid(16);
+
+    $(".grid").mouseover(function() {
+        $(this).css("background-color", "black");
+        });
+
+    $(".newGrid").click(function() {
+        refreshGrid();
+
+        $(".grid").mouseover(function() {
+        $(this).css("background-color", "black");
+        });
+    });
+});
+
